8e650891dffc2956ef7de9b46e5659bef8d3f6cd
Shyam Venkat <shyam.venkat@turner.com>
2020-01-09 20:50:23 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/shyam81992/go-projectA
+
+go 1.13
+
+require (
+	github.com/shyam81992/go-projectB v0.0.0-20200109145349-f8a9ed88cfdf
+	github.com/shyam81992/go-projectC v0.0.0-20200109134841-fc1018db207d
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	projectb "github.com/shyam81992/go-projectB"
+	projectc "github.com/shyam81992/go-projectC"
+)
+
+func main() {
+
+	projectc.PrintMessage()
+	projectb.PrintMessage()
+	fmt.Println("message")
+}
